4f1081fc5f4ddfbe0ab52e4d0da9a87b8cb00f7b
James Kennedy <james_w_kennedy@dell.com>
2013-05-01 11:30:17 -0700

    fixed a string error

--- velo-elevation-editor.js
+++ velo-elevation-editor.js
@@ -132,7 +132,7 @@ function process_TPX_Data(req, res) {
 function display_form(req, res) {
     res.writeHead(200, {"Content-Type": "text/html"});
     res.write(
-        'Enter GPS coordinates to get Google''s elevation data.' +
+        'Enter GPS coordinates to get Google\'s elevation data.' +
         '<form action="/" method="get">'+
         '<input type="input" name="where" value="Latitude/Longitude">'+
         '<input type="submit" value="Get elevation">'+
