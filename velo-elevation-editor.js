07e7fd07d94a5e96f625ce7aa0c471459082d980
James Kennedy <james_w_kennedy@dell.com>
2013-05-01 10:24:56 -0700

    Minor test

--- velo-elevation-editor.js
+++ velo-elevation-editor.js
@@ -13,6 +13,8 @@ var GOOGLE_HOST = 'maps.googleapis.com';
 var GOOGLE_PATH = '/maps/api/elevation/json';
 
 var server = http.createServer(function (request, response) {
+  return;
+
   response.writeHead(200, {'Content-Type': 'text/plain'});
   
   var url_parts = url.parse(request.url, true);
